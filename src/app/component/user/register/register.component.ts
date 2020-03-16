5a813bb90704f2ba7b0ebb95a2fa9820485a21e1
thuyenkhongben <54319302+thuyenkhongben@users.noreply.github.com>
2020-03-16 15:50:34 +0700

    register user

--- src/app/component/user/register/register.component.ts
+++ src/app/component/user/register/register.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../../interface/user';
 import {Login} from '../../../interface/login';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../../service/user.service';
 import {Router} from '@angular/router';
 import {CookieService} from 'ngx-cookie-service';
 import {AuthService} from '../../../auth/auth.service';
+import {SignUpInfor} from '../../../auth/SignUpInfor';
+import {SignUpForm} from '../../../interface/signup-form';
+import {THIS_EXPR} from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-register',
@@ -13,68 +16,55 @@ import {AuthService} from '../../../auth/auth.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  user: User;
-  login: Login;
-  registerForm: FormGroup;
-  check = '';
-  checklaimk = '';
-  constructor(private registerFormBuilder: FormBuilder, private userService: UserService, private authService: AuthService) { }
-
-  checkPassword( group: FormGroup) {
-    const password1 = group.get('password');
-    const password2 = group.get('confirmPassword');
-    return password1 === password2 ? null : { notSame: true};
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required , Validators.minLength(2)]),
+    username: new FormControl('', [Validators.required , Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required , Validators.email]),
+    password: new FormControl('', [ Validators.required , Validators.minLength(3)]),
+    confirmPassword: new FormControl('', [ Validators.required , Validators.minLength(3)]),
+  });
+  signUpForm: SignUpForm;
+  isSignedUp = false;
+  isSigUPFailed = false;
+  errorMessage = '';
+  successSignUp = false;
+  isNotSigUp = ' Wrong Username or Password';
+  displaySigUp = false;
+  message = 'the passwords do not match';
+  displayMessage = false;
+  constructor(private authService: AuthService , private router: Router) {
   }
   ngOnInit() {
-    this.registerForm = this.registerFormBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-      username: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
-      confirmPassword: ['', Validators.required],
-    }, { validators: this.checkPassword}
-    );
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required , Validators.minLength(2)]),
+      username: new FormControl('', [Validators.required , Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required , Validators.email]),
+      password: new FormControl('', [ Validators.required , Validators.minLength(3)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(3)])
+    });
   }
 
-  // onSubmit() {
-  //   this.formUrl();
-  //   this.userService.createUser(this.user).subscribe( next => {
-  //     this.login = { username: this.registerForm.get('username').value , password: this.registerForm.get('password').value};
-  //     this.authService.userSignin(this.login)
-  //     window.sessionStorage.setItem('password' , this.registerForm.get('password').value);
-  //     this.check = 'true';
-  //   },
-  //     error => {
-  //     this.check = 'false';
-  //     }
-  //     );
-  // }
+  onSubmit() {
+    const {name , username , email , password , confirmPassword} = this.form.value;
+    this.signUpForm = new SignUpForm(name , username , email , password);
 
-  formUrl() {
-    this.user = {
-      name: this.registerForm.get('name').value,
-      username: this.registerForm.get('username').value,
-      email: this.registerForm.get('email').value,
-      password: this.registerForm.get('password').value,
-      role: ['admin']
-    };
-  }
-
-  get email() {
-    return this.registerForm.get('email');
-  }
-
-  get username() {
-    return this.registerForm.get('username');
-  }
-  get password() {
-    return this.registerForm.get('password');
-  }
-  get name() {
-    return this.registerForm.get('name');
-  }
-  get confirmPassword() {
-    return this.registerForm.get('confirmPassword');
+    if (password === confirmPassword) {
+      this.authService.signUp(this.signUpForm).subscribe(
+        data => {
+          this.isSignedUp = true;
+          this.isSigUPFailed = false;
+          this.successSignUp = true;
+          this.router.navigate(['api/login']).then(r => console.log(data)
+          );
+        }, error => {
+          this.errorMessage = error.error.message;
+          this.isSigUPFailed = true;
+          this.displaySigUp = true;
+        }
+      );
+    } else {
+      this.displayMessage = true;
+    }
   }
 
 }
